33f51f7da972fa37efa1db2adfaf84f9f9d7f638
freakbyte <freakbyte@gmail.com>
2018-11-13 20:16:05 +0100

    Initial commit

---
+++ Bitter/BinaryWrapper.cs
@@ -0,0 +1,150 @@
+﻿using System.IO;
+using static Bitter.BinaryStream;
+
+namespace Bitter
+{
+    public class BinaryWrapper
+    {
+        private Endianness bitOrder = Endianness.LittleEndian;
+        private Endianness byteOrder = Endianness.LittleEndian;
+        private TextEncoding defaultTextEncoding = TextEncoding.DEFAULT;
+        protected bool keepReadOpen = false;
+        protected bool keepWriteOpen = false;
+
+        /// <summary>
+        /// Order of the bits to be read & written in.
+        /// </summary>
+        public Endianness BitOrder
+        {
+            get { return bitOrder; }
+            set { bitOrder = value; }
+        }
+
+        /// <summary>
+        /// Order of the bytes to be read & written in.
+        /// </summary>
+        public Endianness ByteOrder
+        {
+            get { return byteOrder; }
+            set { byteOrder = value; }
+        }
+
+        /// <summary>
+        /// Default encoding to be used on string.
+        /// </summary>
+        public TextEncoding DefaultTextEncoding
+        {
+            get { return defaultTextEncoding; }
+            set
+            {
+                defaultTextEncoding = value;
+            }
+        }
+
+        /// <summary>
+        /// Reads a binary structure from a file.
+        /// </summary>
+        public void Read(string file)
+        {
+            BinaryStream bs = new BinaryStream(File.Open(file, FileMode.Open), byteOrder, bitOrder, defaultTextEncoding);
+            Read(bs);
+            if(!keepReadOpen)
+            {
+                bs.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Reads a binary structure from a stream.
+        /// </summary>
+        public void Read(Stream stream)
+        {
+            BinaryStream bs = new BinaryStream(stream, byteOrder, bitOrder, defaultTextEncoding);
+            Read(bs);
+            if (!keepReadOpen)
+            {
+                bs.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Reads a binary structure from a byte array.
+        /// </summary>
+        public void Read(byte[] bytes)
+        {
+            BinaryStream bs = new BinaryStream(new MemoryStream(bytes), byteOrder, bitOrder, defaultTextEncoding);
+            Read(bs);
+            if (!keepReadOpen)
+            {
+                bs.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Reads a binary structure from a BinaryStream. Override this one.
+        /// </summary>
+        public virtual void Read(BinaryStream bs)
+        {
+
+        }
+
+        /// <summary>
+        /// Writes a binary structure to a file.
+        /// </summary>
+        public void Write(string file)
+        {
+            BinaryStream bs = new BinaryStream(File.Open(file, FileMode.OpenOrCreate), byteOrder, bitOrder, defaultTextEncoding);
+            Write(bs);
+            if (!keepWriteOpen)
+            {
+                bs.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Writes a binary structure to a stream.
+        /// </summary>
+        public void Write(Stream stream)
+        {
+            BinaryStream bs = new BinaryStream(stream, byteOrder, bitOrder, defaultTextEncoding);
+            Write(bs);
+            if (!keepWriteOpen)
+            {
+                bs.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Writes a binary structure to a byte array.
+        /// </summary>
+        public void Write(out byte[] bytes)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            using (BinaryStream bs = new BinaryStream(ms, byteOrder, bitOrder, defaultTextEncoding))
+            {
+                Write(bs);
+                bytes = ms.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Writes a binary structure to a BinaryStream. Override this one.
+        /// </summary>
+        public virtual void Write(BinaryStream bs)
+        {
+
+        }
+
+        public interface ReadWrite
+        {
+            void Read(BinaryStream bs);
+            void Write(BinaryStream bs);
+        }
+
+        public interface ReadWriteInitialize : ReadWrite
+        {
+            void Init(object parameters = null);
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,74 @@
+
+# BinaryStream
+*.NET Standard 2.0 Library for manipulating binary data*
+
+**The library supports reading and writing**
+ - Induvidual bits
+ - Integers of variable bit length
+ - Signed Byte
+ - Signed Short
+ - Signed Int
+ - Signed Long
+ - Unsigned Byte
+ - Unsigned Short
+ - Unsigned Int
+ - Unsigned Long
+ - Half-precision floating-point numbers
+ - Float
+ - Double
+ - Char
+ - String
+ - Arrays and Lists of everything above.
+
+You can read and write to the **same stream**, making it easy to descramble data without the need of multiple streams.
+
+### Quick usage guide, more will follow 
+```c#
+BinaryStream stream = new BinaryStream();       // if you dont provide it with a stream it'll use a MemoryStream internally.
+
+stream.BitOrder = Endianness.LittleEndian;      // you can set both bit and byte order like this
+stream.ByteOrder = Endianness.LittleEndian;     // or through the constructor
+stream.DefaultTextEncoding = TextEncoding.UTF8; // what text encoding should we use as default, 
+                                                // this can be set through the constructor.
+
+Reader read = stream.Read;                      // you can still use stream.Read.Type
+Writer write = stream.Write;                    // i just think this makes my code easier to read
+
+byte[] bytes = new byte[] { 1, 2, 4, 7 };
+int[] ints = new int[] { -1, 200, 7600, 984, -999 };
+List<float> floats = new List<float>(new float[] { 1f, -2f, 0.55f });
+
+// write
+write.ULong(123456);
+write.ByteArray(bytes);
+write.IntArrayAsBits(ints, 14);
+write.HalfList(floats);
+write.Bit(1);                   // a bit is represented by a byte
+write.Bit(0);
+write.Bit(1);
+write.Bit(0);
+write.String("test");
+
+// reset pos
+stream.Flush();                 // it's important to flush if you write bits
+stream.ByteOffset = 0;          // changing offset will also flush dirty bits
+stream.BitOffset = 0;           // writing data that is not aligned to a byte is a lot slower
+
+// read
+read.ULong();
+read.ByteArray(4);
+read.ShortListFromBits(ints.Length, 14);    // we wrote ints, but can read shorts
+read.HalfArray(floats.Count);               // halfs are converted and returned as floats
+read.BitArray(4);
+read.String(4);
+
+// seek
+stream.BitOffset += 200;
+stream.BitOffset--;
+stream.ByteOffset = 55;
+stream.ByteOffset++;
+
+
+bool end = stream.EndOfStream;
+long len = stream.Length;
+```
